6a42a4b840dac3c6d112f98003651546f79d9ba5
Aleksey Pavlovskii <agp251093@gmail.com>
2025-09-14 19:34:53 +0300

    fix

--- src/decorators.py
+++ src/decorators.py
@@ -1,13 +1,16 @@
 from functools import wraps
 from typing import Any, Callable
 
+import os
+
 
 def write_reports_to_csv(func: Callable) -> Any:
-    """Декоратор для записи отчёта в CSV файл"""
-
-    @wraps(func)
-    def wrapper(*args: tuple, **kwargs: dict) -> Any:
+    """
+    Декоратор, который записывает отчет в CSV файл
+    """
+    def wrapper(*args: tuple, **kwargs: dict):
         result = func(*args, **kwargs)
+        os.makedirs('data', exist_ok=True)
         result.to_csv('data/reports.csv', index=False, encoding='utf-8-sig')
         return result
     return wrapper
