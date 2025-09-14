d3eccbe80ef42e53fbc4d562d15d1f406bf6cf7b
Aleksey Pavlovskii <agp251093@gmail.com>
2025-09-14 12:32:23 +0300

    Добавлена функция для анализа выгодных категорий кэшбэка

---
+++ src/services.py
@@ -0,0 +1,29 @@
+import json
+
+import pandas as pd
+
+
+def analyze_cashback(file_path: str, year: int, month: int) -> str:
+    """
+    Анализирует выгодные категории кэшбэка и возвращает json строку с выгодными категориями кэшбэка
+    """
+
+    df = pd.read_excel(file_path)
+    df["Дата операции"] = pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
+    filtered_data = df[
+        (df["Дата операции"].dt.year == year) & (df["Дата операции"].dt.month == month) & (df["Кэшбэк"] > 0)
+        ]
+
+    expenses_by_category = filtered_data.groupby("Категория")["Кэшбэк"].sum()
+
+    data = {}
+    for i, row in expenses_by_category.items():
+        data[i] = row
+
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+
+    return json_data
+
+
+if __name__ == "__main__":
+    analyze_cashback("../data/operations.xlsx", 2021, 5)
